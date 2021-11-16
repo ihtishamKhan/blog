67124abc2670f86dd30b78b0d3ff38834babf58f
ihtishamKhan <ihtisham11051@gmail.com>
2021-11-16 17:05:51 +0500

    Port hardcoded

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const cors = require('cors');
 const proxy = require('express-http-proxy');
 const app = express();
-const { PORT } = process.env;
+// const { PORT } = process.env;
 
 app.use(cors());
 app.use(express.json());
@@ -16,6 +16,6 @@ app.use('/api/posts', proxy('http://127.0.0.1:8001'));
 app.use('/comments', proxy('http://127.0.0.1:8002'));
 
 
-app.listen(PORT, () => {
-    console.log(`Gateway is listening to ${PORT}`);
+app.listen(8000, () => {
+    console.log(`Gateway is listening to 8000`);
 })
