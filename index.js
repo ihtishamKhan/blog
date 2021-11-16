bf0e9c957982658dbf31beddcb0b9db95a59ebc6
ihtishamKhan <ihtisham11051@gmail.com>
2021-11-16 17:17:25 +0500

    Home changes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
     return res.status(200).json({ "msg": "Hello form Gateway" });
 });
 
